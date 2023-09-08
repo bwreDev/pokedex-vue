a0bc8be55d9c60a8f547b1f7a9d1e4798a78a319
Colton Bonner <cbonner26118@gmail.com>
2023-09-08 17:42:38 -0600

    Connect to api. Create grid of 60 pokemon. Create individual routes for each pokemon. Fetch pokemon information for each route. Style pokemon grid and individual page.

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink class="link" to="/">Home</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+.link {
+  text-decoration: none;
+  color: #212223;
+  font-weight: 900;
+}
+</style>
